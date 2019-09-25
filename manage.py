9889204417f748ff1d017a4d6af53fa1aa3e66b5
pascaline <passyish5@gmail.com>
2019-09-25 16:21:22 +0200

    imported all the classes in manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,PhotoProfile,Review
+from app.models import User,PhotoProfile,Review,Post,Comment,Upvote,Downvote
 from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 app = create_app('development')
@@ -21,7 +21,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Review =Review)
+    return dict(app = app,db = db,User = User,Review =Review,Upvote = Upvote,Post = Post Comment = Comment,Downvote = Downvote)
 
 if __name__ == '__main__':
     manager.run()
